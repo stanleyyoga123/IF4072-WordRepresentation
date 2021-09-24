f0974980d604961e54af85dcfc586e64f4aaa3aa
William9923 <williamong1400@gmail.com>
2021-09-25 00:17:02 +0700

    improvement(w2v-no-context): adding sample script for tryout

---
+++ run-w2v-example.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "----------------------------------"
+echo "📝 Preparing..."
+echo "🧪 exp1 : CBOW | window : 5 | learning rate : 0.001 "
+python main.py -w2v -n exp1 -lr 0.001 -bs 64 -e 5 -t 0 -w 5 -msl 128
+echo "🧪 exp1 : CBOW | window : 5 | learning rate : 0.001 w/ Fasttext"
+python main.py -w2v -n exp1 -lr 0.001 -bs 64 -e 5 -t 0 -w 5 -msl 128
+echo "✅ Training complete. Happy learning ♥️."
--- run-w2v.sh
+++ run-w2v.sh
@@ -2,6 +2,36 @@
 
 echo "----------------------------------"
 echo "📝 Preparing..."
-echo "🧪 exp1 : CBOW | window : 5 | learning rate : 0.001 (Replicate previous experiment)"
+echo "🧪 exp1 : CBOW | window : 5 | learning rate : 0.001 "
+python main.py -w2v -n exp1 -lr 0.001 -bs 64 -e 5 -t 0 -w 5 -msl 128
+echo "🧪 exp3 : CBOW | window : 5 | learning rate : 0.0001 "
+python main.py -w2v -n exp1 -lr 0.001 -bs 64 -e 5 -t 0 -w 5 -msl 128
+echo "🧪 exp4 : CBOW | window : 3 | learning rate : 0.001 "
+python main.py -w2v -n exp1 -lr 0.001 -bs 64 -e 5 -t 0 -w 5 -msl 128
+echo "🧪 exp6 : CBOW | window : 3 | learning rate : 0.0001 "
+python main.py -w2v -n exp1 -lr 0.001 -bs 64 -e 5 -t 0 -w 5 -msl 128
+echo "🧪 exp10 : SG | window : 5 | learning rate : 0.001 "
+python main.py -w2v -n exp1 -lr 0.001 -bs 64 -e 5 -t 0 -w 5 -msl 128
+echo "🧪 exp12 : SG | window : 5 | learning rate : 0.0001 "
+python main.py -w2v -n exp1 -lr 0.001 -bs 64 -e 5 -t 0 -w 5 -msl 128
+echo "🧪 exp13 : SG | window : 3 | learning rate : 0.001 "
+python main.py -w2v -n exp1 -lr 0.001 -bs 64 -e 5 -t 0 -w 5 -msl 128
+echo "🧪 exp15 : SG | window : 3 | learning rate : 0.0001 "
+python main.py -w2v -n exp1 -lr 0.001 -bs 64 -e 5 -t 0 -w 5 -msl 128
+echo "🧪 exp1 : CBOW | window : 5 | learning rate : 0.001 w/ Fasttext"
+python main.py -w2v -n exp1 -lr 0.001 -bs 64 -e 5 -t 0 -w 5 -msl 128
+echo "🧪 exp3 : CBOW | window : 5 | learning rate : 0.0001 w/ Fasttext"
+python main.py -w2v -n exp1 -lr 0.001 -bs 64 -e 5 -t 0 -w 5 -msl 128
+echo "🧪 exp4 : CBOW | window : 3 | learning rate : 0.001 w/ Fasttext"
+python main.py -w2v -n exp1 -lr 0.001 -bs 64 -e 5 -t 0 -w 5 -msl 128
+echo "🧪 exp6 : CBOW | window : 3 | learning rate : 0.0001 w/ Fasttext"
+python main.py -w2v -n exp1 -lr 0.001 -bs 64 -e 5 -t 0 -w 5 -msl 128
+echo "🧪 exp10 : SG | window : 5 | learning rate : 0.001 w/ Fasttext"
+python main.py -w2v -n exp1 -lr 0.001 -bs 64 -e 5 -t 0 -w 5 -msl 128
+echo "🧪 exp12 : SG | window : 5 | learning rate : 0.0001 w/ Fasttext"
+python main.py -w2v -n exp1 -lr 0.001 -bs 64 -e 5 -t 0 -w 5 -msl 128
+echo "🧪 exp13 : SG | window : 3 | learning rate : 0.001 w/ Fasttext"
+python main.py -w2v -n exp1 -lr 0.001 -bs 64 -e 5 -t 0 -w 5 -msl 128
+echo "🧪 exp15 : SG | window : 3 | learning rate : 0.0001 w/ Fasttext"
 python main.py -w2v -n exp1 -lr 0.001 -bs 64 -e 5 -t 0 -w 5 -msl 128
 echo "✅ Training complete. Happy learning ♥️."
